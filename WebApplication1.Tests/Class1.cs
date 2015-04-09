619de7a4d4a4651560f6b1aac92a511b98f0ce8a
1101137101 <p19857@gmail.com>
2015-04-09 14:48:07 +0800

    thirteenth_commit

---
+++ WebApplication1.Tests/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApplication1.Tests
+{
+    public class Class1
+    {
+        [TestFixture]
+        public class Class2
+        {
+            [Test]
+            public void CalYear()
+            {
+                Born.Class1 cal = new Born.Class1();
+                int inputAge = 30;
+                int expect = 1988;
+
+                Assert.AreEqual(expect, cal.GetBirthYear(inputAge, BO.Cal.YearFormat.W));
+            }
+        }
+    }
+}
