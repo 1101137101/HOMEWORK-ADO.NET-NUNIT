50b2ae65ac7cccff023fdf91c5bd5a4554fb93e0
1101137101 <p19857@gmail.com>
2015-04-09 14:02:28 +0800

    tenth_commit

---
+++ Birth/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birth
+{
+    public class Class1
+    {
+        public enum YearFormat
+        {
+            c = 0, // 民國
+            w = 1  // 西元
+        }
+        public int GetBirthYear(int Age, YearFormat format)
+        {
+            int WestYear = (DateTime.Now.Year - Age);
+
+            if (format == YearFormat.c)
+                return (WestYear - 1911);
+            else if (format == YearFormat.w)
+                return WestYear;
+
+            return 0;
+        }
+    }
+}
